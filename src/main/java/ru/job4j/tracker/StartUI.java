ca0aad296d43dcac2cf55ba930eef255900a3a81
Aleksey VV <avbavnn@gmail.com>
2020-10-23 21:16:14 +0300

    5. Tracker - хранилище [#354756]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -14,8 +14,13 @@ public class StartUI {
         String currentDateTimeFormat = currentDT.format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
         System.out.println(item);
-    }
 
+        Item itemNoOne = new Item(1, "One");
+        Tracker trackerNoOne = new Tracker();
+        Item alarmItem = trackerNoOne.add(itemNoOne);
+        System.out.println(trackerNoOne.findById(1));
+        System.out.println(trackerNoOne.findById(10));
 
     }
+}
 
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -10,6 +12,25 @@ public class Tracker {
         items[size++] = item;
         return item;
     }
+
+    public Item[] findAll() {
+        Item[] findAll = Arrays.copyOf(items, size);
+        return  findAll;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] findByName = new Item[100];
+        int count = 0;
+        for (int i = 0; i < items.length; i++) {
+            String name = items[i].getName();
+            if (name.equals(key)){
+                findByName[count] = items[i];
+                count++;
+            }
+        }
+        return findByName = Arrays.copyOf(findByName, count) ;
+    }
+
 
     public Item findById(int id) {
         Item rsl = null;
@@ -22,4 +43,5 @@ public class Tracker {
         }
         return rsl;
     }
+
 }
